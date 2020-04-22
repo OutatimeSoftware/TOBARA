655fd11a4ef2bb33371560e389ec4e32ea9ec36a
Apoquinto <A16003312@alumnos.uady.mx>
2020-04-22 15:52:52 -0500

    Check

--- main.py
+++ main.py
@@ -7,43 +7,63 @@ def getFun():
 	return boolFun.lower()
 	
 def getVar( fun ):
+	
 	tam = len( fun )
+	
 	menor = mayor = ord( fun[ 0 ] )
+	
 	for i in range( tam ):
-		if ( ord(fun[i]) > mayor ):
+	
+		if ( ord(fun[i]) > mayor and ord(fun[i]) <= 122 ):
+	
 			mayor = ord(fun[i])
-		elif ( ord(fun[i]) < menor ):
+	
+		elif ( ord(fun[i]) < menor and ord(fun[i]) >= 97 ):
+	
 			menor = ord(fun[i])
+
 	return mayor - menor + 1
 	
-def getMen( fun ):
-	tam = len( fun )
-	menor = ord( fun[ 0 ] )
-	for i in range( tam ):
-		if ( ord(fun[i]) < menor ):
-			menor = ord(fun[i])
-	return menor + 1
 	
+
 def getTer( fun ):
-	tam = len( fun )
-	terminos = 1
-	for i in range( tam ):
-		if ( fun[i] == "+" ):
-			terminos += 1
+
+    terminos = 1
+
+    for variable in fun:
+        if (variable == '+'):
+            terminos += 1
+            
+    return terminos
 			
 def generateTable( boolfun, var ):
-	print(var)
+	
 	table = []
-	for i in range( 2 ** var  ):
-		table.append( [0] * ( var + 1 ) )
+	
+	tam = y = 2 ** var 
+	
+	for i in range( y ):
+	
+		table.append([])
+		
+		for j in range( var + 1 ):
+			
+			table[ i ].append( 0 )		
+	
 	return table
 	
 def printTable( table, var ):
+
 	for i in range( 2 ** var ):
+	
 		for j in range( var + 1 ):
+	
 			if( j >= var ):
+	
 				print("|", end = " ")
+	
 			print(table[i][j], end = " ")
+	
 		print()
 	
 if __name__ == "__main__":
