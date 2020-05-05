f12fd60166ea1ab45c40a3539cf62ad34d6c5348
Apoquinto <A16003312@alumnos.uady.mx>
2020-05-05 14:51:27 -0500

    Anexo de aplicación para Kmaps

---
+++ apps/Kmaps.py
@@ -0,0 +1,62 @@
+import pygame
+
+import numpy as np
+
+def main():
+
+	# inicializa pygame
+
+	pygame.init()
+
+	# titulo de la ventana
+
+	pygame.display.set_caption('K-map')
+
+	#Definicion de colores
+
+	bg = (220, 220, 220)
+	cyan = (0, 255, 255)
+
+	#Declaracion de variables
+	
+	celdasX, celdasY = 4, 4
+	
+	#Creaciond de pantalla
+
+	width, height = 100 * celdasX, 100 * celdasY
+	screen = pygame.display.set_mode((width, height))
+	screen.fill(bg)
+	
+	# Actualizacion de pantalla
+	
+	pygame.display.flip()
+
+	# bucle infinito
+	while ( True ):
+    	# obtiene un solo evento de la cola de eventos
+		event = pygame.event.wait()
+		
+		#Creacion de la cadricula
+		
+		for y in range( 0, celdasY ):
+			for x in range( 0, celdasX ):
+				
+				poly = [( ( x ) * 100, ( y ) * 100 ),
+						( ( x + 1 ) * 100, ( y ) * 100 ),
+						( ( x + 1 ) * 100, ( y + 1 ) * 100 ),
+						( ( x ) * 100, ( y + 1 ) * 100 ) ]
+				
+				pygame.draw.polygon( screen, ( 160, 160, 160 ), poly, 1 )
+
+		pygame.display.flip()
+
+		# si se presiona el botón 'cerrar' de la ventana
+		if event.type == pygame.QUIT:
+			# detiene la aplicación
+			break
+
+	# finaliza Pygame
+	pygame.quit()
+
+if __name__ == '__main__':
+    main()
