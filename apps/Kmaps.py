59567226213f467a93690cd27ea8876b9f6e507e
Apoquinto <A16003312@alumnos.uady.mx>
2020-05-21 19:02:49 -0500

    Hola

--- apps/Kmaps.py
+++ apps/Kmaps.py
@@ -1,6 +1,6 @@
 import pygame
-
 import numpy as np
+import time
 
 def main():
 
@@ -8,26 +8,28 @@ def main():
 
 	pygame.init()
 
-	# titulo de la ventana
+	# título de la ventana
 
-	pygame.display.set_caption('K-map')
+	pygame.display.set_caption('The Game Of Life')
 
-	#Definicion de colores
+	#Definición de colores
 
-	bg = (220, 220, 220)
-	cyan = (0, 255, 255)
+	bgFalse = ( 220, 220, 220 )
+	border = ( 160, 160, 160 )
+	bgTrue = ( 220, 237, 160 )
 
-	#Declaracion de variables
+	#Declaración de variables
 	
-	celdasX, celdasY = 4, 4
+	varX, varY = 4, 4
+	cellSize = 50
 	
-	kmap = np.zeros( ( celdasX, celdasY ) )
+	kmap = np.zeros( ( varX, varY ) )
 	
-	#Creacion de pantalla
+	#Creación de pantalla
 
-	width, height = 100 * celdasX, 100 * celdasY
-	screen = pygame.display.set_mode((width, height))
-	screen.fill(bg)
+	width, height = varX * cellSize, varY * cellSize 
+	screen = pygame.display.set_mode( ( width, height ) )
+	screen.fill( bgFalse )
 	
 	# Actualizacion de pantalla
 	
@@ -35,20 +37,41 @@ def main():
 
 	# bucle infinito
 	while ( True ):
-    	# obtiene un solo evento de la cola de eventos
+    	# Obtiene un solo evento de la cola de eventos
 		event = pygame.event.wait()
 		
+		ev = pygame.event.get()
+		
+		for event in ev:
+			
+			#Detectar interacci[on con el mouse
+			mouseClick = pygame.mouse.get_pressed()
+			
+			if( sum(mouseClick) > 0 ):
+			
+				posX, posY = pygame.mouse.get_pos()
+				
+				celX, CelY = int( posX / cellSize ), int( posY / cellSize )
+				
+				kmap[ celX, CelY ] = not mouseClick[ 2 ]
+				
 		#Creacion de la cadricula
 		
-		for y in range( 0, celdasY ):
-			for x in range( 0, celdasX ):
+		for y in range( 0, varY ):
+		
+			for x in range( 0, varX ):
 				
-				poly = [( ( x ) * 100, ( y ) * 100 ),
-						( ( x + 1 ) * 100, ( y ) * 100 ),
-						( ( x + 1 ) * 100, ( y + 1 ) * 100 ),
-						( ( x ) * 100, ( y + 1 ) * 100 ) ]
+				poly = [( ( x ) * cellSize, ( y ) * cellSize ),
+						( ( x + 1 ) * cellSize, ( y ) * cellSize ),
+						( ( x + 1 ) * cellSize, ( y + 1 ) * cellSize ),
+						( ( x ) * cellSize, ( y + 1 ) * cellSize ) ]
 				
-				pygame.draw.polygon( screen, ( 160, 160, 160 ), poly, 1 )
+				if( kmap[ x, y ] == 0 ):
+					pygame.draw.polygon( screen, bgFalse, poly, 0 )
+					pygame.draw.polygon( screen, border, poly, 1 )
+				else:
+					pygame.draw.polygon( screen, bgTrue, poly, 0 )
+					pygame.draw.polygon( screen, border, poly, 1 )
 
 		pygame.display.flip()
 
