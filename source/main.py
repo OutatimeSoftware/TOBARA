5faf343f5639ad26c2983fa2c08ed5a1a81db963
Apoquinto <A16003312@alumnos.uady.mx>
2020-05-27 16:54:49 -0500

    Modify reduceKmap

--- source/main.py
+++ source/main.py
@@ -1,50 +1,42 @@
 from func_getBin import *
 from func_getKmap import *
-from func_getTable import *
-from func_getMinTable import *
+
+def reduceKmap( kmap ):
 	
+	active = kmap
+	
+	x = 0
+	
+	y = 0
+	
+	for line in active:
+		
+		y += 1 
+		
+		x = 0
+		
+		for cell in line:
+		
+			x += 1
+		
+			if( cell == 1 ):
+				
+				print( x, y )
+	
+def detectRectangle( kmap, x, y ):
+	
+	
+	
+
 def main():
 
 	# lista de mintérminos.
-	fun = [ 0, 3, 12, 15 ];
+	fun = [ 0, 4 ]
 	var = 4
-	Kmap = getKmap( var, fun )
-	table = getTable( var, fun )
-	minTable = getMinTable( var, fun )
 	
-	print(  )
+	kmap = getKmap( var, fun )
 	
-	print( "Los mintérminos de la función ingresada son: {}".format( fun ) )
-	
-	print(  )
-	
-	print( "La tabla de verdad de la función es: " )
-	
-	for i in table:
-		
-		print( "", end = " " )
-		
-		print( i )
-
-	print(  )
-
-	print( "La tabla de mintérminos de la función es:" )
-	
-	for i in minTable:
-		
-		print( "", end = " " )
-		
-		print( i )
-	
-	print(  )
-	
-	print( "El Kmap de la función es: " )
-
-	for i in Kmap:
-		
-		print( "", end = " " )
-		
-		print( i )
+	reduceKmap( kmap )
 	
 if __name__ == "__main__":
 
