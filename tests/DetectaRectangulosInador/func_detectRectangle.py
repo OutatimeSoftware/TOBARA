1419ce7261c2ff7452d3eccd791f665e07241fdb
Apoquinto <A16003312@alumnos.uady.mx>
2020-06-08 20:58:50 -0500

    Agregado revisión de lineas horizontales y verticales

--- tests/DetectaRectangulosInador/func_detectRectangle.py
+++ tests/DetectaRectangulosInador/func_detectRectangle.py
@@ -8,7 +8,7 @@ def reduceKmap( kmap ):
 	x = 0
 	
 	y = 0
-	
+	"""
 	for line in mask:
 		
 		y += 1 
@@ -19,37 +19,84 @@ def reduceKmap( kmap ):
 		
 			if( cell == 1 ):
 				
-				detectRectangle( kmap, mask, x, y )
+				detectRectangle( kmap, mask, x, y, 1 )
+	"""
+	# detectRectangle( kmap, mask, x, y, 1 )
+	
+	#Comprobación de la linea Horizontal de ( 3, 3 )
+	print( checkLineH( kmap, 2 ) )
+	#Comprobación de la linea Vertical de ( 3, 3 )
+	print( checkLineV( kmap, 2 ) )
 			
-def	checkLineH( kmap, ref, start, end ):
+def	checkLineH( kmap, ref ):
 	
-	pass
+	x = ref
 	
+	while( x > 0 ):
+	
+		if( kmap[ ref ][ x ] != 1 ):
 
-def detectRectangle( kmap, mask, x, y ):
+			print( x + 1, ref + 1 )
+
+			return False			
+			
+		x -= 1
+		
+	return True
+	
+def	checkLineV( kmap, ref ):
+	
+	x = ref
+	
+	while( x > 0 ):
+	
+		if( kmap[ x ][ ref ] != 1 ):
+
+			print( ref + 1 , x + 1 )
+
+			return False			
+			
+		x -= 1
+		
+	return True
+
+def detectRectangle( kmap, mask, x, y, var ):
+	
+	origenX = x
+	
+	origenY = y
 	
 	rect = [ ( x, y ), ( x, y ) ]
 	
-	while( kmap[ x + 1  ][ y + 1 ] ):
+	if( x == y ):
 		
-		 x += 1
-		 y += 1
-		
-		print( "Active" )
-	
-	#Extra para ver cuantas veces entra la función
-	print( x, y )
-	
-	
+		"""
+		while( kmap[ x ][ y ] == 1 ):
+			
+			if ( checkLineH( kmap, x ) and checkLineV( kmap, y ) ):
+			
+				print( x, y )
+			
+			x += 1
+			
+			y += 1
+		"""
 
 def main():
 
 	# lista de mintérminos.
-	fun = [ 0, 4 ]
+	#Ambos falsos
+	fun = [ 0, 1, 2, 4, 5, 8, 10 ]
+	#Ambos verdaderos
+	#fun = [ 0, 1, 2, 4, 5, 6, 8, 9, 10 ]
 	
 	var = 4
 	
 	kmap = getKmap( var, fun )
+	
+	for line in kmap:
+		
+		print( line )
 	
 	reduceKmap( kmap )
 	
